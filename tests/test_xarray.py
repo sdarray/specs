4da21502c721c5d7f37f1af5adb104b6a0856010
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2019-12-17 13:41:24 +0900

    Update test script

--- tests/test_xarray.py
+++ tests/test_xarray.py
@@ -7,8 +7,15 @@ import toml
 
 # constants
 PATH = Path(os.environ["PROJECT_PATH"]) / "xarray"
+KEYS_REQUIRED = {"dtype", "default", "required", "doc"}
+KEYS_OPTIONAL = {"dims", "option"}
 
 
 def test_sdarray(filename="sdarray.toml"):
     with open(PATH / filename) as f:
-        toml.load(f)
+        specs = toml.load(f)
+
+    for item in specs.values():
+        keys = set(item.keys())
+        assert KEYS_REQUIRED <= keys
+        assert (keys - KEYS_REQUIRED) <= KEYS_OPTIONAL
